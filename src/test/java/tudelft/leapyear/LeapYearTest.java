95891b6310171b1e091757ff7fa2aa6bbf702177
drG0n20 <marcinster@gmail.com>
2023-02-01 18:20:07 +0100

    Write the two tests we miss

--- src/test/java/tudelft/leapyear/LeapYearTest.java
+++ src/test/java/tudelft/leapyear/LeapYearTest.java
@@ -28,4 +28,10 @@ public class LeapYearTest {
         LeapYear ly = new LeapYear();
         Assertions.assertFalse(ly.isLeapYear(2017));
     }
+
+    @Test
+    public void nonLeapYearsNegative() {
+        LeapYear ly = new LeapYear();
+        Assertions.assertFalse(ly.isLeapYear(-2017));
+    }
 }
