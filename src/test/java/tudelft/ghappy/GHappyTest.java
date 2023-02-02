62814fbbb25e3fdb612c78c37a30cdeeedd9fbbf
drG0n20 <marcinster@gmail.com>
2023-02-02 17:48:08 +0100

    Add tests and one bug found

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,35 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    @Test
+    public void noHappyG() {
+        GHappy happy = new GHappy();
+        boolean result = happy.gHappy("xxggxx");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void yesHappyG() {
+        GHappy happy = new GHappy();
+        boolean result = happy.gHappy("xxgxx");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void twoGHappyG() {
+        GHappy happy = new GHappy();
+        boolean result = happy.gHappy("xxggyygxx");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void treeHappy() {
+        GHappy happy = new GHappy();
+        boolean result = happy.gHappy("sagggsf");
+        Assertions.assertTrue(result);
+    }
 }
