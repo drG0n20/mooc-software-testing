10495db4a28d894b708f1fd9251bc79c6d520ce2
drG0n20 <marcinster@gmail.com>
2023-02-06 17:27:42 +0100

    Write 2 tests for caesar cipher, find bug - 2xsb.append

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,23 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import tudelft.ghappy.GHappy;
+
 public class CaesarShiftCipherTest {
+
+    @Test
+    public void abcCase() {
+        CaesarShiftCipher lower = new CaesarShiftCipher();
+        String result = lower.CaesarShiftCipher("abc", 3);
+        Assertions.assertEquals(result, "def");
+    }
+
+    @Test
+    public void xyzCase() {
+        CaesarShiftCipher lower = new CaesarShiftCipher();
+        String result = lower.CaesarShiftCipher("xyz", 3);
+        Assertions.assertEquals(result, "abc");
+    }
 
 }
